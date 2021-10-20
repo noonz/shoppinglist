d2a0bb98ce142d0f9b908bd24b7f504f9368a4b8
dave nunez <noonz2@icloud.com>
2021-10-20 04:03:07 +0000

    implement backend

--- client/src/reducers/ItemReducer.js
+++ client/src/reducers/ItemReducer.js
@@ -1,31 +1,34 @@
 import { v4 as uuid } from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Lettuce'},
-        { id: uuid(), name: 'Chicken'},
-        { id: uuid(), name: 'Ranch'},
-        { id: uuid(), name: 'Tomatoes'},
-        { id: uuid(), name: 'Bacon'}
-    ]
+    items: [],
+    loading: false
+
 }
 
 export default function(state = initialState, action) {
     switch(action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             }
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !== action.payload)
+                items: state.items.filter(item => item._id !== action.payload)
             }
         case ADD_ITEM:
             return {
                 ...state,
                 items: [action.payload, ...state.items]
+            }
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             }
         default:
             return state;
